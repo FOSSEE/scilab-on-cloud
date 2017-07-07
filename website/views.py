40d46edc14bd672aa69af04d50cad2214e3aba6b
prashantsinalkar <prashantsinalkar@gmail.com>
2017-07-07 12:25:25 +0530

    added dynamic category fetch

--- website/views.py
+++ website/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render 
 from django.core.context_processors import csrf
+from website.models import *
 
 def index(request):
     context = {}
     return render(request, 'website/templates/index.html', context)
+
+def catg(cat_id):
+    category = TextbookCompanionCategoryList.objects.using('scilab').get(id=cat_id)
+    return category.category_name
