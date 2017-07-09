aec110c8254db4f984262da2c03b14b8b706fad9
prashantsinalkar <prashantsinalkar@gmail.com>
2017-07-09 21:20:40 +0530

    modifed the config file

--- soc/config.py
+++ soc/config.py
@@ -1,24 +1,37 @@
-DB_NAME_DEFAULT = 'soc'                                                                      
-DB_USER_DEFAULT = ''
-                                                                     
-DB_PASS_DEFAULT = ''   
-                                                              
-DB_HOST_DEFAULT = ''                                                                         
-DB_PORT_DEFAULT = ''                                                                         
+DB_NAME_DEFAULT='default_db'
+DB_USER_DEFAULT='db user'
+DB_PASS_DEFAULT='db pass'
+DB_HOST_DEFAULT=''
+DB_PORT_DEFAULT=''
 
+DB_NAME_SCILAB='external db'
+DB_USER_SCILAB='external db user'
+DB_PASS_SCILAB='external db pass'
+DB_HOST_SCILAB=''
+DB_PORT_SCILAB=''
 
-DB_NAME_SCILAB = ''
-
-DB_USER_SCILAB = ''
-                                                                      
-DB_PASS_SCILAB = '' 
-                                                                 
-DB_HOST_SCILAB = ''                                                                          
-DB_PORT_SCILAB = ''                                                                          
-
-SCILAB_BIN = '/home/scilab_test/CLOUD/scilab/scilab-5.4.1/bin/scilab-adv-cli'
+BIN = 'scilab directory path'
 #SCILAB_FLAGS = '-noatomsautoload'
-SCILAB_FLAGS = '-noatomsautoload -nogui -nb ' #srikant
-SCIMAX_LOADER = '/home/scilab_test/CLOUD/scilab/scilab-scimax-2.1.4/loader.sce'
-UPLOADS_PATH = '/home/scilab_test/CLOUD/uploads'
+SCILAB_FLAGS = '-noatomsautoload -nogui -nb '
+SCIMAX_LOADER = 'scimax directory path/loader.sce'
+UPLOADS_PATH = 'uploads directory path'
+SCILAB_3= 'scilab-5.3.3'
+SCILAB_4= 'scilab-5.4.1'
+SCILAB_5= 'scilab-5.5.2'
+
+# Host for sending e-mail.
+EMAIL_HOST_SERVER = 'smtp server'
+
+# Port for sending e-mail.
+EMAIL_PORT_SERVER = 587
+
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER_SERVER = 'user'
+EMAIL_HOST_PASSWORD_SERVER = 'password'
+EMAIL_USE_TLS_SERVER = True
+
+FROM_EMAIL = 'from'
+TO_EMAIL = 'to'
+CC_EMAIL = 'cc'
+BCC_EMAIL = 'bcc'
 
