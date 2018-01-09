b099fbebf93b1745fcaa217ad31864734cc6681a
prashantsinalkar <prashantsinalkar@gmail.com>
2018-01-09 16:31:38 +0530

    added urls for search books

--- website/urls.py
+++ website/urls.py
@@ -5,10 +5,16 @@ from website import ajax
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'website.views.index', name='index'),
+    url(r'^$', 'website.views.landing', name='landing'),
     url(r'^index$', 'website.views.index', name='index'),
     url('', include('django.contrib.auth.urls', namespace='auth')),
-    url(r'^submit-revision/$', 'website.ajax.revision_form', name='revision_form'),
+    url(r'^submit-revision/$', 'website.ajax.revision_form',
+        name='revision_form'),
+    url(r'^update_view_count/$', 'website.views.update_view_count',
+        name='update_view_count'),
+    url(r'^search_book/$', 'website.views.search_book'),
+    url(r'^search_book/popular/$', 'website.views.popular'),
+    url(r'^search_book/recent/$', 'website.views.recent'),
 
     # Uncomment the next line to enable the admin:
     #url(r'^admin/', include(admin.site.urls)),
@@ -19,7 +25,7 @@ urlpatterns = patterns(
     # for review interface
     url(r'^review/$', 'website.views.review', name='review'),
 
-    url(r'^reviewer-login/$', 'django.contrib.auth.views.login', \
+    url(r'^reviewer-login/$', 'django.contrib.auth.views.login',\
         {'template_name': 'admin/login.html'}),
     #url(r'^logout/$', 'website.views.logout', name='logout'),
 )
