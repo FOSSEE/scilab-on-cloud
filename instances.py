a5d0bed3bc2890dc97a706e3e0bba7501829f1cb
prashantsinalkar <prashantsinalkar@gmail.com>
2018-01-09 16:22:33 +0530

    added feature generate log and fixed code for pep8

--- instances.py
+++ instances.py
@@ -5,37 +5,48 @@ import re
 import time
 import sys
 
+from datetime import datetime
 # importing the local variables
 from soc.settings import PROJECT_DIR
-from soc.config import BIN, SCILAB_FLAGS, SCIMAX_LOADER, UPLOADS_PATH,\
-    SCILAB_3, SCILAB_4, SCILAB_5
+from soc.config import (BIN, SCILAB_FLAGS, SCIMAX_LOADER, UPLOADS_PATH,
+                        SCILAB_3, SCILAB_4, SCILAB_5)
+from website.models import (TextbookCompanionCategoryList, ScilabCloudComment,
+                            TextbookCompanionSubCategoryList,
+                            TextbookCompanionProposal,
+                            TextbookCompanionRevision,
+                            TextbookCompanionPreference,
+                            TextbookCompanionChapter, TextbookCompanionExample,
+                            TextbookCompanionRevision,
+                            TextbookCompanionExampleFiles,
+                            TextbookCompanionExampleDependency,
+                            TextbookCompanionDependencyFiles)
 
-''' An object of class ScilabInstance handles spawning and maintaining of multiple
-scilab instances. 
+''' An object of class ScilabInstance handles spawning and maintaining of
+ multiple scilab instances.
 
-maxsize is the upper bound of number of Scilab instances that can be alive at the 
-same time.
+maxsize is the upper bound of number of Scilab instances that can be alive at
+ the same time.
 
 instances list maintains a pool of free Scilab instances.
 
-count is the number of Scilab instances alive currently. 
+count is the number of Scilab instances alive currently.
 
-spawn_instance method is used to create Pexpect objects, which in turn spawn Scilab
-instance. A new instance is spawned only if the count is not exceeding the value of 
-maxsize. 
+spawn_instance method is used to create Pexpect objects, which in turn spawn
+ Scilab instance. A new instance is spawned only if the count is not exceeding
+ the value of maxsize.
 
-kill_instances method is used to kill the free Scilab instances in the list instances,
-based on the parameter count passed while invoking the method.
+kill_instances method is used to kill the free Scilab instances in the list
+ instances, based on the parameter count passed while invoking the method.
 
-get_available_instance method is used to fetch a non-busy Scilab instance. 
-It receives one from the list instances or else invokes spawn_instances method to
-fetch a new instance to execute the Scilab code. If there are no Scilab instances
-available, the request has to wait until an instance is available.
+get_available_instance method is used to fetch a non-busy Scilab instance.
+It receives one from the list instances or else invokes spawn_instances method
+ to fetch a new instance to execute the Scilab code. If there are no Scilab
+ instances available, the request has to wait until an instance is available.
 
 execute_code method executes the code passed as one of its parameter. It invokes
 get_available_instance method, fetches a Scilab instance and executes the code.
-After the execution of the code, the Pexpect object containing Scilab instance is 
-put back into the instances list. 
+After the execution of the code, the Pexpect object containing Scilab instance
+ is put back into the instances list.
 '''
 
 
@@ -69,7 +80,8 @@ class ScilabInstance(object):
             instance.close()
             self.count -= 1
 
-    # returns an active_instancescilab instance. This will block till it gets an active_instance.
+    # returns an active_instancescilab instance. This will block till it gets an
+    # active_instance.
     def get_available_instance(self):
         if not self.instances and self.count < self.maxsize:
             self.spawn_instance()
@@ -77,13 +89,16 @@ class ScilabInstance(object):
             pass
         return self.instances.pop(0)
 
-    def execute_code(self, code, token, book_id, dependency_exists, chapter_id, example_id):
+    def execute_code(
+            self, code, token, book_id, dependency_exists, chapter_id,
+            example_id):
 
         # Check for system commands
-        print "okk"
         # print code, token, book_id, dependency_exists
         system_commands = re.compile(
-            'unix\(.*\)|unix_g\(.*\)|unix_w\(.*\)|unix_x\(.*\)|unix_s\(.*\)|host|newfun|execstr|ascii|mputl|dir\(\)'
+            'unix\(.*\)|unix_g\(.*\)|unix_w\(.*\)|'
+            'unix_x\(.*\)|unix_s\(.*\)|host|newfun'
+            '|execstr|ascii|mputl|dir\(\)'
         )
         if system_commands.search(code):
             return {
@@ -104,7 +119,7 @@ class ScilabInstance(object):
         # Finding the plot and appending xs2jpg function
         #p = re.compile(r'.*plot.*\(.*\).*\n|bode\(.*\)|evans\(.*\)')
         p = re.compile(
-            r'plot*|.*plot.*\(.*\).*\n|bode\(.*\)|stem\(.*\)|evans\(.*\)')
+            r'plot|.*plot.*\(.*\).*\n|bode\(.*\)|stem\(.*\)|evans\(.*\)')
 
         plot_path = ''
         if p.search(code):
@@ -129,7 +144,8 @@ class ScilabInstance(object):
         f.write('driver("PNG");\n')
         f.write('xinit("{0}");\n'.format(plot_path))
         f.write('mode(2);\n')
-        if dependency_exists == True and book_id != 0 and chapter_id != 0 and example_id != 0:
+        if dependency_exists == True and book_id != 0 and chapter_id != 0 \
+                and example_id != 0:
             f.write(
                 'cd("{0}/{1}/DEPENDENCIES/");\n'.format(UPLOADS_PATH, book_id)
             )
@@ -150,8 +166,8 @@ class ScilabInstance(object):
             self.instances.append(active_instance)
 
         except:
-            active_instance.before += "Exception Occured: It seems that you are running \
-                                        an infinite code"
+            active_instance.before += "Exception Occured: It seems that you are\
+            running an infinite code"
             output = self.trim(active_instance.before)
             active_instance.close()
             self.count -= 1
@@ -162,6 +178,30 @@ class ScilabInstance(object):
             'output': output,
             'plot_path': plot_path.replace(PROJECT_DIR, '')
         }
+        now = datetime.now()
+        log_file_name = now.strftime("%Y-%m-%d")
+        if book_id != 0 and chapter_id != 0 and example_id != 0:
+            book = TextbookCompanionPreference.objects.using('scilab')\
+                .filter(id=book_id)
+            chapter = TextbookCompanionChapter.objects.using('scilab')\
+                .filter(id=chapter_id)
+            example = TextbookCompanionExample.objects.using('scilab')\
+                .filter(id=example_id)
+            f = open(PROJECT_DIR + '/static/log/' +
+                     str(log_file_name) + '.txt', "a")
+            f.write("************************************" + "\n")
+            f.write(str(datetime.now()) + "\n")
+            f.write("------------------------------------" + "\n")
+            f.write("Book: {0} [Author: {1}]\n".format(
+                book[0].book, book[0].author))
+            f.write("Chapter: {0}\n".format(chapter[0].name))
+            f.write("Example: ({0}) {1}\n".format(
+                example[0].number, example[0].caption))
+            f.write("------------------------------------" + "\n")
+            f.write("Output" + "\n")
+            f.write("------------------------------------" + "\n")
+            f.write(output + "\n")
+            f.write("************************************" + "\n")
         return data
 
     def trim(self, output):
