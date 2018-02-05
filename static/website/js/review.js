c30f8699fbb9638bdb91fbae62ef32c950934de8
prashantsinalkar <prashantsinalkar@gmail.com>
2018-02-05 20:07:15 +0530

    removed dajax, formatted code and replaced with jquery

--- static/website/js/review.js
+++ static/website/js/review.js
@@ -1,32 +1,40 @@
 $(document).ready(function() {
 
-	var reviewEditor = CodeMirror.fromTextArea(document.getElementById("review-code"), {
+    var reviewEditor = CodeMirror.fromTextArea(document.getElementById(
+        "review-code"), {
         lineNumbers: true,
         lineWrapping: true,
         theme: "monokai",
         readOnly: true,
         extraKeys: {
-           "F11": function(cm) {
-            cm.setOption("fullScreen", !cm.getOption("fullScreen"));
-           },
-           "Esc": function(cm) {
-            if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
-           }
-         }
+            "F11": function(cm) {
+                cm.setOption("fullScreen", !cm.getOption(
+                    "fullScreen"));
+            },
+            "Esc": function(cm) {
+                if (cm.getOption("fullScreen")) cm
+                    .setOption("fullScreen",
+                        false);
+            }
+        }
     });
 
-    var reviewResult = CodeMirror.fromTextArea(document.getElementById("review-result"), {
+    var reviewResult = CodeMirror.fromTextArea(document.getElementById(
+        "review-result"), {
         lineWrapping: true,
         theme: "monokai",
         readOnly: true,
         extraKeys: {
-           "F11": function(cm) {
-            cm.setOption("fullScreen", !cm.getOption("fullScreen"));
-           },
-           "Esc": function(cm) {
-            if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
-           }
-         }
+            "F11": function(cm) {
+                cm.setOption("fullScreen", !cm.getOption(
+                    "fullScreen"));
+            },
+            "Esc": function(cm) {
+                if (cm.getOption("fullScreen")) cm
+                    .setOption("fullScreen",
+                        false);
+            }
+        }
     });
 
     /* Code Mirror Controls */
@@ -34,15 +42,17 @@ $(document).ready(function() {
     $toggle_code = $("#review-toggle-code");
 
     $fullscreen_code.click(function(e) {
-        reviewEditor.setOption("fullScreen", !reviewEditor.getOption("fullScreen"));
+        reviewEditor.setOption("fullScreen", !
+            reviewEditor.getOption("fullScreen"));
         reviewEditor.focus();
         e.preventDefault();
     });
 
     $toggle_code.click(function(e) {
-        if(reviewEditor.getOption("theme") == "monokai") {
+        if (reviewEditor.getOption("theme") ==
+            "monokai") {
             reviewEditor.setOption("theme", "default");
-        } else{
+        } else {
             reviewEditor.setOption("theme", "monokai");
         }
         e.preventDefault();
@@ -52,15 +62,17 @@ $(document).ready(function() {
     $toggle_result = $("#review-toggle-result");
 
     $fullscreen_result.click(function(e) {
-        reviewResult.setOption("fullScreen", !reviewResult.getOption("fullScreen"));
+        reviewResult.setOption("fullScreen", !
+            reviewResult.getOption("fullScreen"));
         reviewResult.focus();
         e.preventDefault();
     });
 
     $toggle_result.click(function(e) {
-        if(reviewResult.getOption("theme") == "monokai") {
+        if (reviewResult.getOption("theme") ==
+            "monokai") {
             reviewResult.setOption("theme", "default");
-        } else{
+        } else {
             reviewResult.setOption("theme", "monokai");
         }
         e.preventDefault();
@@ -70,38 +82,47 @@ $(document).ready(function() {
         console.log(reviewEditor.getValue());
 
         var send_data = {
-            token: $("[name='csrfmiddlewaretoken']").val(),
+            token: $(
+                "[name='csrfmiddlewaretoken']"
+            ).val(),
             code: reviewEditor.getValue(),
             book_id: $("#books").val() || 0,
             chapter_id: $("#chapters").val() || 0,
             example_id: $("#examples").val() || 0
         };
         $.post("/execute-code", send_data,
-        function(data){
-            $("#execute-inner").html("Execute");
-            reviewResult.setValue(data.output);
+            function(data) {
+                $("#execute-inner").html(
+                    "Execute");
+                reviewResult.setValue(data.output);
 
-            if(data.plot_path){
-                $plot = $("<img>");
-                $plot.attr({
-                    src: data.plot_path,
-                    width: '90%'
-                });
-                $plotbox.html($plot);
-                $plotbox_wrapper.lightbox_me({
-                    centered: true
-                });
-                var dt = $("#examples option:selected").text();
-                $("#plot_download").show();
-                $("#plot_download").attr("download", dt+'.png');
-                $("#plot_download").attr("href", data.plot_path);
-            }
-        });
+                if (data.plot_path) {
+                    $plot = $("<img>");
+                    $plot.attr({
+                        src: data.plot_path,
+                        width: '90%'
+                    });
+                    $plotbox.html($plot);
+                    $plotbox_wrapper.lightbox_me({
+                        centered: true
+                    });
+                    var dt = $(
+                        "#examples option:selected"
+                    ).text();
+                    $("#plot_download").show();
+                    $("#plot_download").attr(
+                        "download", dt +
+                        '.png');
+                    $("#plot_download").attr(
+                        "href", data.plot_path
+                    );
+                }
+            });
     });
 
-     /* Ajax loader */
+    /* Ajax loader */
     function ajax_loader(key) {
-        if(key == "clear") {
+        if (key == "clear") {
             $(".ajax-loader").remove();
         } else {
             $(".ajax-loader").remove();
@@ -110,11 +131,11 @@ $(document).ready(function() {
     }
 
     $("#review-control-buttons").hide()
-	
+
     //  -----------------------------------------------------
 
     // callback when revision selection changes
-	$(document).on("change", "#review-revisions", function() {
+    $(document).on("change", "#review-revisions", function() {
 
         $("#category").html("")
         $("#book").html("")
@@ -126,41 +147,87 @@ $(document).ready(function() {
         reviewEditor.setValue("")
         reviewResult.setValue("")
 
-        if ($(this).val()) { 
-    		ajax_loader(this);
-    		Dajaxice.website.review_revision(function(data) {
-                reviewEditor.setValue(data.code)
-                console.log(data)
-                $("#category").html(`<span><strong>Category: </strong></span>` + data.category)
-                $("#book").html(`<span><strong>Textbook: </strong></span>` + data.book.book)
-                $("#chapter").html(`<span><strong>Chapter: </strong></span>` + data.chapter.name)
-                $("#example").html(`<span><strong>Example: </strong></span>` + data.example.caption)
-                $("#commit-message").html(`<span><strong>Commit Message: </strong></span>` + data.revision.commit_message)
+        if ($(this).val()) {
+            ajax_loader(this);
+            $.ajax({
+                url: 'get_review_revision/',
+                dataType: 'JSON',
+                type: 'GET',
+                data: {
+                    revision_id: $(this).val()
+                },
+                success: function(data) {
+                    reviewEditor.setValue(
+                        data.code)
+                    console.log(data)
+                    $("#category").html(
+                        `<span><strong>Category: </strong></span>` +
+                        data.category
+                    )
+                    $("#book").html(
+                        `<span><strong>Textbook: </strong></span>` +
+                        data.book.book
+                    )
+                    $("#chapter").html(
+                        `<span><strong>Chapter: </strong></span>` +
+                        data.chapter.name
+                    )
+                    $("#example").html(
+                        `<span><strong>Example: </strong></span>` +
+                        data.example.caption
+                    )
+                    $("#commit-message").html(
+                        `<span><strong>Commit Message: </strong></span>` +
+                        data.revision
+                        .commit_message
+                    )
 
-                $("#review-control-buttons").show()
+                    $(
+                        "#review-control-buttons"
+                    ).show()
 
-                ajax_loader("clear");
-            }, {revision_id: $(this).val()});
+                    ajax_loader("clear");
+                }
+            });
         }
-	});
+    });
 
     // callback on pressing push button
     $(document).on("click", "#push", function() {
         $(this).html("pushing..")
-        Dajaxice.website.push_revision(function(data) {
-            Dajax.process(data);
-            $(this).html('Push revision')
-        }, {code: reviewEditor.getValue(),}
-        );
+
+        $.ajax({
+            url: 'get_push_revision/',
+            dataType: 'JSON',
+            type: 'GET',
+            data: {
+                code: reviewEditor.getValue(),
+            },
+            success: function(data) {
+                $(this).html(
+                    'Push revision')
+                alert(data);
+                location.reload();
+            }
+        });
     });
 
     // callback on pressing remove button
     $(document).on("click", "#remove", function() {
         $(this).html("removing..")
-        Dajaxice.website.remove_revision(function(data) {
-            Dajax.process(data);
-            $(this).html('Remove revision')
-        }
-        );
+
+        $.ajax({
+            url: 'get_remove_revision/',
+            dataType: 'JSON',
+            type: 'GET',
+            data: {},
+            success: function(data) {
+                $(this).html(
+                    'Remove revision'
+                );
+                alert(data);
+                location.reload();
+            }
+        });
     });
 });
