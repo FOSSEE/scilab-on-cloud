3237051295f7a4a6876751804ed5da3dc207b642
prashantsinalkar <prashantsinalkar@gmail.com>
2018-09-04 12:11:33 +0530

    updated the file

--- cloud.start.stop.sh
+++ cloud.start.stop.sh
@@ -2,21 +2,14 @@
 
 # Start
 
-export WORKING_DIR="PATH_to_CLOUD_DIR"
-#SOC_PID=$WORKING_DIR"/scilab.on.cloud.pid"
-export SOC_PID="PATH_to_CLOUD_DIR/tmp/scilab.on.cloud.pid"
+export WORKING_DIR="/scilab-on-cloud"
+export SOC_PID="/tmp/scilab.on.cloud.pid"
 
+#Start
 
 start()
 {
- 
-    #su - scilab.cloud
-    #cd $WORKING_DIR
-    #. ../env_cloud/bin/activate
-    screen -dmS myscilabcloud sh -c "cd $WORKING_DIR ; source PATH_to_python/rh-python36/enable  ; source PATH_to_CLOUD_DIR/scilab-on-cloud_env/bin/activate ; nohup $WORKING_DIR/run.sh"
-    #echo $(ps -eo comm,pid,etimes | awk '/^scilab-adv-cli/ { $1=""; print $0 }') > /run/scilab.on.cloud.pid
-    #sleep 10
-    #echo $(ps -eo comm,pid,etimes | awk '/^scilab-bin/ { print $2 }') > $SOC_PID
+    screen -dmS screencloud sh -c "cd $WORKING_DIR ; source /opt/rh/rh-python36/enable  ; source /soc_env/bin/activate ; nohup $WORKING_DIR/run.sh"
 }
 
 # Stop
@@ -25,8 +18,8 @@ stop()
 {
     kill -15 $(cat $SOC_PID)
     sleep 30
-    screen -X -S myscilabcloud quit
-    #rm -fr $SOC_PID
+    screen -X -S screencloud quit
+
 
 }
 
