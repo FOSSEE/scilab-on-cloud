3bd2c90abbe812fc57155348d460a05fd279592e
prashantsinalkar <prashantsinalkar@gmail.com>
2018-09-18 10:52:59 +0530

    added robots file in template

--- soc/urls.py
+++ soc/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('website.urls')),
+    re_path(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots_file"),
     path('admin/', admin.site.urls),
 ]
